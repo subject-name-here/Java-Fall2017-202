88d6632c1554bee428850f01596d3873c657acca
subject-name-here <jura.paradov@gmail.com>
2018-03-06 22:05:14 +0300

    Added implementation of thenApply method in ThreadTask, wrote javadocs, added enum for status of computation in LightFuture.

--- hw10/src/main/java/ru/spbau/mit/java/paradov/LightFuture.java
+++ hw10/src/main/java/ru/spbau/mit/java/paradov/LightFuture.java
@@ -2,11 +2,29 @@ package ru.spbau.mit.java.paradov;
 
 import java.util.function.Function;
 
+/**
+ * Interface for tasks that were added in ThreadPoolImpl.
+ * @param <T> type of returning value
+ */
 public interface LightFuture<T> {
-
+    /**
+     * Checks if computation of task is over, and result can be returned.
+     * @return true if result is ready, false otherwise
+     */
     public boolean isReady();
 
+    /**
+     * Returns result of computation, when it's done.
+     * @return result of computation
+     * @throws LightExecutionException if providing supplier has finished work with exception
+     */
     public T get() throws LightExecutionException;
 
-    public <R> LightFuture<R> thenApply(Function<T, R> function);
+    /**
+     * Takes Function object, then applies it to result of this LightFuture and returns new
+     * LightFuture object as a task accepted in ThreadPool.
+     * @param function function to apply to this function's result
+     * @return new LightFuture object as a task accepted in ThreadPool
+     */
+    public LightFuture<T> thenApply(Function<T, T> function);
 }
--- hw10/src/main/java/ru/spbau/mit/java/paradov/ThreadPoolImpl.java
+++ hw10/src/main/java/ru/spbau/mit/java/paradov/ThreadPoolImpl.java
@@ -6,18 +6,38 @@ import java.util.Queue;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Task pool with fixed number of threads (number is given in constructor).
+ * Every thread is waiting for a task, then it computes some value and returns to waiting.
+ * @param <T> type of tasks results
+ */
 public class ThreadPoolImpl<T> {
+    /**
+     * ArrayList of threads. Once ThreadPool is constructed, this list is filled with
+     * running threads.
+     */
     private final ArrayList<Thread> threads = new ArrayList<>();
+    /**
+     * Task pool, where we can add tasks to and where from threads take tasks.
+     */
     private final Queue<ThreadTask> tasks = new LinkedList<>();
 
+    /**
+     * Constructs new ThreadPool with given number of running threads.
+     * @param n number of threads to run in ThreadPool
+     */
     public ThreadPoolImpl(int n) {
         for (int i = 0; i < n; i++) {
             threads.add(new Thread(new TaskDealer()));
-            // Should I make it daemon???
             threads.get(i).start();
         }
     }
 
+    /**
+     * Adds given supplier as a task to ThreadPool.
+     * @param supplier task to be added to the pool
+     * @return object LightFuture - wrap around supplier - added task
+     */
     public LightFuture<T> addTask(Supplier<T> supplier) {
         ThreadTask newTask = new ThreadTask(supplier);
 
@@ -29,59 +49,114 @@ public class ThreadPoolImpl<T> {
         return newTask;
     }
 
+    /**
+     * Interrupts all threads in this ThreadPool. Changes flag, so all LightFuture objects,
+     * which wait for result, know that result will not be provided.
+     */
     public void shutdown() {
         for (Thread t : threads) {
             t.interrupt();
         }
     }
 
+    /**
+     * Implementation of LightFuture - class for tasks that are taken by this ThreadPool.
+     */
     private class ThreadTask implements LightFuture<T> {
+        /** Supplier that provides value to compute. */
         private Supplier<T> supplier;
-        private boolean isReady = false;
-        private boolean isFinishedWithException = false;
+        /** Status of computation. It can be READY, NOT_READY or FINISHED_WITH_EXCEPTION. */
+        private LightFutureStatus status = LightFutureStatus.NOT_READY;
+        /** It is either a result provided by supplier or exception thrown by it. */
         private Object result;
 
+        /** Monitor object to warn method get() when computation is done. */
+        private final Object monitor = new Object();
+
+        /**
+         * Constructs task from given supplier.
+         * @param supplier supplier that provides value to compute
+         */
         private ThreadTask(Supplier<T> supplier) {
             this.supplier = supplier;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public boolean isReady() {
-            return isReady;
+            return status == LightFutureStatus.READY;
         }
 
+        /**
+         * Returns result of computation, when it's done. If thread was interrupted while
+         * waiting, it saves exception as a result.
+         * @return result of computation
+         * @throws LightExecutionException if providing supplier has finished work with exception
+         */
         @Override
         public T get() throws LightExecutionException {
-            while (!isReady) {
-                // What to do, if someone shutdown ThreadPool while result was computing?
+            synchronized (monitor) {
+                try {
+                    while (!isReady()) {
+                        monitor.wait();
+                    }
+                } catch (InterruptedException e) {
+                    status = LightFutureStatus.FINISHED_WITH_EXCEPTION;
+                    result = new Exception("Computation wasn't completed: " +
+                            "ThreadPool was shutdown.");
+                }
             }
 
-            if (isFinishedWithException) {
+            if (status == LightFutureStatus.FINISHED_WITH_EXCEPTION) {
                 throw new LightExecutionException((Exception) result);
             }
 
             return (T) result;
         }
 
+        /**
+         * {@inheritDoc}
+         */
         @Override
-        public <R> LightFuture<R> thenApply(Function<T, R> function) {
-            return null;
+        public LightFuture<T> thenApply(Function<T, T> function) {
+            return ThreadPoolImpl.this.addTask(() -> {
+                try {
+                    return function.apply(get());
+                } catch (LightExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
 
+        /**
+         * Gets value from supplier and saves it. If it throws an exception, then it saves
+         * this exception as a result.
+         */
         private void computeResult() {
             try {
                 result = supplier.get();
             } catch (Exception e) {
                 result = e;
-                isFinishedWithException = true;
+                status = LightFutureStatus.FINISHED_WITH_EXCEPTION;
             }
 
-            isReady = true;
+            status = LightFutureStatus.READY;
+            monitor.notify();
         }
     }
 
 
+    /**
+     * Runnable implementation for ThreadPoolImpl to run. They are waiting for tasks,
+     * and when they grabbed one, they compute it and wait again.
+     */
     private class TaskDealer implements Runnable {
+
+        /**
+         * {@inheritDoc}
+         */
         @Override
         public void run() {
             while (!Thread.interrupted()) {
@@ -98,7 +173,6 @@ public class ThreadPoolImpl<T> {
 
                     task = tasks.remove();
                 }
-
 
                 task.computeResult();
             }
