8b26fe3a9b3f9fc214b2a9a0e21ece4e33bed90e
subject-name-here <jura.paradov@gmail.com>
2018-05-04 10:47:53 +0300

    added more tests

--- hw13/src/test/java/ru/spbau/mit/java/paradov/ClientTest.java
+++ hw13/src/test/java/ru/spbau/mit/java/paradov/ClientTest.java
@@ -1,5 +1,7 @@
 package ru.spbau.mit.java.paradov;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -10,13 +12,22 @@ import static org.junit.Assert.*;
 
 public class ClientTest {
     private static final int PORT_NUMBER = 22229;
+    private static Thread serverThread;
+
+    @BeforeClass
+    public static void setUpServer() {
+        Server server = new Server(22229);
+        serverThread = new Thread(server::run);
+        serverThread.start();
+    }
+
+    @AfterClass
+    public static void killServer() {
+        serverThread.interrupt();
+    }
 
     @Test
     public void testGetList() {
-        Server server = new Server(22229);
-        Thread serverThread = new Thread(server::run);
-        serverThread.start();
-
         String query = "1 src" + File.separator + "test" + File.separator + "resources" + File.separator + "dir1\n e";
         ByteArrayInputStream bais = new ByteArrayInputStream(query.getBytes());
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -27,7 +38,48 @@ public class ClientTest {
         String expected = "3\n" + "folder true\n" + "kek1 false\n" + "kek2 false\n\n";
 
         assertEquals(expected, baos.toString());
-
-        serverThread.interrupt();
     }
+
+    @Test
+    public void testGetEmptyList() {
+        String query = "1 src" + File.separator + "test" + File.separator + "resources" + File.separator + "dir0\n e";
+        ByteArrayInputStream bais = new ByteArrayInputStream(query.getBytes());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Client client = new Client("localhost", PORT_NUMBER);
+        client.run(bais, baos);
+
+        String expected = "0\n\n";
+
+        assertEquals(expected, baos.toString());
+    }
+
+    @Test
+    public void testGetList2() {
+        String query = "1 src" + File.separator + "test" + File.separator + "resources" + File.separator + "dir2\n e";
+        ByteArrayInputStream bais = new ByteArrayInputStream(query.getBytes());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Client client = new Client("localhost", PORT_NUMBER);
+        client.run(bais, baos);
+
+        String expected = "7\n" + "f1 false\n" + "f2 false\n" + "finf false\n"
+                + "k1 true\n" + "k2 true\n" + "k3 true\n"
+                + "the file false\n\n";
+
+        assertEquals(expected, baos.toString());
+    }
+/*
+    public void testGetFile() {
+        String query = "2 src/test/resources/dir1/kek1".replace("/", File.separator);
+        ByteArrayInputStream bais = new ByteArrayInputStream(query.getBytes());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        Client client = new Client("localhost", PORT_NUMBER);
+        client.run(bais, baos);
+
+
+
+        assertEquals(expected, baos.toString());
+    }*/
 }
